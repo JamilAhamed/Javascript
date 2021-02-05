183d561e000a7e829318a6db33c4daf8f9562827
Your Name <you@example.com>
2021-02-05 11:13:23 +0600

    module 25 ending

---
+++ new-js/timeout.js
@@ -0,0 +1,16 @@
+function doSomething(){
+    console.log(3333)
+}
+
+console.log(2222);
+// setTimeout(() =>{
+// console.log('see you later.')
+// }, 4000)
+setInterval(function() {
+    console.log('doing it')
+}, 4000);
+
+console.log(4444);
+console.log(4444);
+console.log(4444);
+console.log(4444);
